28c6e488ef835067c4e7693dcd6d65a4aa1f9294
Artem Lajko <artem.lajko@hpa.hamburg.de>
2023-09-18 15:29:01 +0200

    add output to ref private_key

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "network_security_group_name" {
   value       = azurerm_network_security_group.main.name
   description = "value of the network security group name"
 }
+
+output "tls_private_key" {
+  value       = tls_private_key.main.private_key_pem
+  description = "value of the tls private key"
+}
